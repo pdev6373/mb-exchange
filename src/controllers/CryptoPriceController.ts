d07e2b4eb5917866901818f04b843deb66683a88
pdev6373 <adebayoluborode@gmail.com>
2025-03-23 09:37:03 +0100

    update

--- src/controllers/CryptoPriceController.ts
+++ src/controllers/CryptoPriceController.ts
@@ -192,7 +192,7 @@ export function initWebSocketServer(server: Server): void {
 async function fetchHistoricalDataFromCoinbase(
   symbol: string,
 ): Promise<CandleData[]> {
-  const granularity = 21600;
+  const granularity = 3600;
   const end = Math.floor(Date.now() / 1000);
   const start = end - 24 * 60 * 60;
 
