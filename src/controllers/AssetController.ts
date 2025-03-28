32c886ad0c10f78be35ffb04d071af97cedc71c6
pdev6373 <adebayoluborode@gmail.com>
2025-03-28 10:51:49 +0100

    update

--- src/controllers/AssetController.ts
+++ src/controllers/AssetController.ts
@@ -29,7 +29,6 @@ import { TransactionStatus } from '../schemas/user';
 export class AssetController {
   @Get('/')
   public async getAllAssets() {
-    await TransactionModel.deleteMany();
     const assets = await AssetModel.find().sort({ createdAt: -1 }).lean();
     return successResponse('Assets fetched successfully', assets);
   }
